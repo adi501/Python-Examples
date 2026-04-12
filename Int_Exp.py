bb61f06500e64cf73e921ae509460e26d8f5132a
Adinarayana Reddy JC <jc.adi101@gmail.com>
2026-04-12 12:18:15 +0530

    Int Example

---
+++ Int_Exp.py
@@ -0,0 +1,11 @@
+num1=123
+num2=456
+print(num1)
+print(num2)
+print(num1+num2)
+# large numbers
+num3=123456789
+print(num3)
+# you can use underscores (123_456_789) instead of commas (123,456,789) as a visual separator for large numbers
+num4=123_456_789 #  123456789 and 123_456_789 both are same
+print(num4)
