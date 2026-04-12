1c893530628fd55af73e254748c1137cda97639e
Adinarayana Reddy JC <jc.adi101@gmail.com>
2026-04-12 12:17:59 +0530

    String Example

---
+++ String_Exp.py
@@ -0,0 +1,12 @@
+# string
+
+name="adi"
+print(name)
+print(name[0])
+print(len(name))
+print(len("Hello World"))  # o/p: 11
+print("Hello"[0]) # o/p: H  (1st letter)
+print("Hello"[1]) # o/p: e  (2nd letter)
+print("Hello"[2]) # o/p: l  (3rd letter)
+print("Hello"[-1]) # o/p: o  (last letter)
+print("Hello"[-2]) # o/p: l   (2nd last letter)
