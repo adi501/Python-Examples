e5ee97873deb607a2e012165891050ac41818388
Adinarayana Reddy JC <jc.adi101@gmail.com>
2026-04-12 13:24:36 +0530

    Type checking Example

---
+++ Type_Checking_Exp.py
@@ -0,0 +1,11 @@
+#Type Checking
+
+print(type("adi")) # O/P : <class 'str'>
+print(type("123"))  # O/P : <class 'str'>
+print(type(123))  # O/P : <class 'int'>
+print(type(123_456_789))  # O/P : <class 'int'>
+print(type(123456789))  # O/P : <class 'int'>
+print(type(123.45))  # O/P : <class 'float'>
+print(type("True"))  # O/P : <class 'str'>
+print(type(True))  # O/P : <class 'bool'>
+print(type(False))  # O/P : <class 'bool'>
