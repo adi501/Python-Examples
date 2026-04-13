b37d31acae0171b71fc1d35f2ac63752894d757d
Adinarayana Reddy JC <jc.adi101@gmail.com>
2026-04-13 19:45:23 +0530

    If_Else Example

---
+++ If_Else_Exp.py
@@ -0,0 +1,7 @@
+print("Welcome to SSE Exams")
+age = int(input("What is your Age in Years? "))
+
+if age >= 15:
+    print("You can write the SSE Exams")
+else:
+    print("Sorry You Are not eligible to write SSE Exams.")
---
+++ Mathematical_Operators_Exp.py
@@ -0,0 +1,8 @@
+print(123+456) #o/p : 579
+print(5-2)  #o/p : 3
+print(5*2) #o/p : 10
+print(5/2) #o/p : 2.5
+print(5//2) #o/p : 2
+print(5%2) #o/p : 1
+print(3 * 3 + 3 / 3 - 3) #o/p : 7.0
+print(2**3) #o/p : 8 (2 to power 3, 2*2*2)
