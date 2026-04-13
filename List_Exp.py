d20c72a85c084f222765c954d930e5d84d0be3ad
Adinarayana Reddy JC <jc.adi101@gmail.com>
2026-04-13 20:45:29 +0530

    Lists Example

---
+++ List_Exp.py
@@ -0,0 +1,10 @@
+#List
+
+list_of_numbers = [1,2,3,5]
+list_of_numbers[1] = 10  # update Index 1
+list_of_numbers.append(60)  # adding new number
+list_of_numbers.extend([11, 12,13]) # Extending 3 numbers
+print(list_of_numbers[2]) # it will print Index 2 number
+print(list_of_numbers[-1]) # it will print last index number
+print(list_of_numbers[-2]) # it will print 2nd last index number
+print(list_of_numbers)
